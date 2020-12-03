5fbd6f509a8fdcbfa004d7072a0506d23153c7da
Stephan Protsack <stephan@speedandfunction.com>
2020-12-03 15:41:10 +0200

    Added models lookup support

--- model.go
+++ model.go
@@ -1,8 +1,40 @@
 package ores
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
 var support = map[string]map[Model]bool{}
-var models = map[Model]bool{
-	ModelDamaging: true,
+var models = []Model{ModelDamaging}
+
+func init() {
+	info := make(map[string]struct {
+		Models map[Model]ModelInfo `json:"models"`
+	})
+
+	data, err := ioutil.ReadFile("./config/support.json")
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = json.Unmarshal(data, &info)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for dbName, config := range info {
+		support[dbName] = make(map[Model]bool)
+
+		for _, model := range models {
+			if _, exists := config.Models[model]; exists {
+				support[dbName][model] = true
+			}
+		}
+	}
 }
 
 // Supported ores models
@@ -10,10 +42,16 @@ const (
 	ModelDamaging Model = "damaging"
 )
 
+// ModelInfo all the model meta data
+type ModelInfo struct {
+	Version string `json:"version"`
+}
+
 // Model ORES scoring type
 type Model string
 
 // Supports check if model is available for certain database
 func (m Model) Supports(dbName string) bool {
-	return true
+	_, exists := support[dbName][m]
+	return exists
 }
--- response.go
+++ response.go
@@ -1,9 +1,7 @@
 package ores
 
 type response struct {
-	Models map[Model]struct {
-		Version string `json:"version"`
-	} `json:"models"`
+	Models map[Model]ModelInfo `json:"models"`
 	Scores map[int]map[Model]struct {
 		Score map[string]interface{} `json:"score"`
 	} `json:"scores"`

6813dfe0034d51702239e29bd3d240ea77220fb4
Stephan Protsack <stephan@speedandfunction.com>
2020-12-04 12:26:49 +0200

    Error modification

--- response.go
+++ response.go
@@ -1,12 +1,14 @@
 package ores
 
+type responseError struct {
+	Type    string `json:"type"`
+	Message string `json:"message"`
+}
+
 type response struct {
 	Models map[Model]ModelInfo `json:"models"`
 	Scores map[int]map[Model]struct {
-		Error struct {
-			Type    string `json:"type"`
-			Message string `json:"message"`
-		} `json:"error"`
+		Error responseError          `json:"error"`
 		Score map[string]interface{} `json:"score"`
 	} `json:"scores"`
 }
